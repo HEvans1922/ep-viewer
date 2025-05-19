97e2f312a98b5c573ceede8ee21f0837aa8e4f92
HEvans1922 <harveyevans413@outlook.com>
2025-05-19 12:08:49 +0100

    Update custom.js

--- custom/custom.js
+++ custom/custom.js
@@ -1,5 +1,5 @@
 function close_window() {
-    if (confirm("Are you sure you want to close this tab? The document(s) you have open may not be saved.")) {
+    if (confirm("Are you sure you want to close this tab? The document(s) you have open will not be saved!")) {
         close();
     }
 }
